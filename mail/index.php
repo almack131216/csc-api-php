a714e6a963fa975aa3732eca237f8808dc7b8288
ecp-alex <alex.mackenzie@englishcollege.cz>
2020-11-21 06:25:13 +0100

    MAIL | working | sending to sales@ + bcc me

--- mail/index.php
+++ mail/index.php
@@ -34,32 +34,38 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
   if (empty($errors)) {
     $date = date('F j, Y h:i A');
 
-	$emailBody = 'Name: '.$fName.' '.$lName.'<br>';
-	$emailBody .= 'Email: '.$email.'<br>';
-	$emailBody .= 'Contact No: '.$tel.'<br>';
-	$emailBody .= 'Mobile: '.$mobile.'<br>';
-	$emailBody .= 'Address: '.$address.'<br>';
-	$emailBody .= 'Post Code: '.$pCode.'<br>';
-	$emailBody .= '<br>';
-	$emailBody .= 'Make: '.$carMake.'<br>';
-	$emailBody .= 'Model: '.$carModel.'<br>';
-	$emailBody .= 'Year: '.$carYear.'<br>';
-	$emailBody .= 'Colour: '.$carColor.'<br>';
-	$emailBody .= 'Notes: '.$carNotes;
-	$emailBody .= '<br><br>Sent: '.$date;
+$emailBody = 'Name: '.$fName.' '.$lName.'<br>';
+$emailBody .= 'Email: '.$email.'<br>';
+$emailBody .= 'Contact No: '.$tel.'<br>';
+$emailBody .= 'Mobile: '.$mobile.'<br>';
+$emailBody .= 'Address: '.$address.'<br>';
+$emailBody .= 'Post Code: '.$pCode.'<br>';
+$emailBody .= '<br>';
+$emailBody .= 'Make: '.$carMake.'<br>';
+$emailBody .= 'Model: '.$carModel.'<br>';
+$emailBody .= 'Year: '.$carYear.'<br>';
+$emailBody .= 'Colour: '.$carColor.'<br>';
+$emailBody .= 'Notes: '.$carNotes;
+$emailBody .= '<br><br>Sent: '.$date;
 
-    $to = "alex@amactive.net";
+//$emailBody = 'test';
+
+	$to = "sales@classicandsportscar.ltd.uk";
+	$email_from	= "sales@classicandsportscar.ltd.uk";
+	$bcc = "alex@amactive.net";
+	$bcc = "al_2003_1@hotmail.com";
 	//$to = "200224@mailinator.com";
 
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=UTF-8\r\n";
-	$headers .= "From: Website Request Form <" . $to . ">";
-	$headers .= "Reply-To: $email"."\r\n";
+	$headers .= "From: Website Request Form <" . $to . ">\r\n";
+	$headers .= "Reply-To: ".$email."\r\n";
+	$headers .= "Bcc: ".$bcc."\r\n";
 
-
-    $subject = 'Website Car Request';
+    $subject = 'Website Car Request';	
     
-    if (mail($to, $subject, $emailBody, $headers)) {
+	if(@mail($to,$subject,$emailBody,$headers,'-f'.$email_from)){
+    //if (mail($to, $subject, $emailBody, $headers)) {
       $sent = true;	
     }
   }
